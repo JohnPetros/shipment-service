1bd330889088196f1a055e7542e329e6891952d5
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-11-29 16:14:03 +0000

    🔧 chore: add app config

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,4 @@
 import 'dotenv/config'
-import { envConfig } from './configs/envConfig'
-import fastify from 'fastify'
+import { Fastify } from './http/fastify'
 
-const app = fastify()
-
-app.get('/hello', () => {
-  return 'hello, world'
-})
-
-app
-  .listen({
-    port: envConfig.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running on Port: ' + envConfig.PORT)
-  })
+export const app = new Fastify().init()
