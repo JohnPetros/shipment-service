7c69a41ae6ecffde8ed66c29fc8244f80fc10a75
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-11-30 14:48:16 +0000

    🏷️ type: add melhro envio env variables to EnvConfig type

--- src/configs/interfaces/IEnvConfig.ts
+++ src/configs/interfaces/IEnvConfig.ts
@@ -1,4 +1,9 @@
 export interface IEnvConfig {
   PORT: number
   NODE_ENV: string | undefined
+  MELHOR_ENVIO_DEV_URL: string | undefined
+  MELHOR_ENVIO_PROD_URL: string | undefined
+  MELHOR_ENVIO_SECRET: string | undefined
+  MELHOR_ENVIO_CLIENT_ID: number
+  MELHOR_ENVIO_REDIRECT_URI: string | undefined
 }
