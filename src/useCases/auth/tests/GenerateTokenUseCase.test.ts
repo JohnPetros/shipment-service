b29b61d3d4bee01dd1623a74ad5cc76e670ba4e1
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-01 21:17:15 +0000

    🧪 test: generate token

--- src/useCases/auth/tests/GenerateTokenUseCase.test.ts
+++ src/useCases/auth/tests/GenerateTokenUseCase.test.ts
@@ -6,29 +6,30 @@ import { GenerateTokenUseCase } from '../GenerateTokenUseCase'
 import { apiServerMock } from '@providers/HttpClientProvider/mocks/httpServerMock'
 import { afterEach } from 'node:test'
 
-let httpClientProvider: HttpClientProvider 
-let shipmentProvider: ShipmentProvider 
-let generateTokenUseCase: GenerateTokenUseCase 
+let httpClientProvider: HttpClientProvider
+let shipmentProvider: ShipmentProvider
+let generateTokenUseCase: GenerateTokenUseCase
 
 describe('Generate Token Use Case', () => {
   beforeAll(() => apiServerMock.listen())
   beforeEach(async () => {
-    httpClientProvider = new HttpClientProvider() 
-    shipmentProvider = new ShipmentProvider(httpClientProvider) 
-    generateTokenUseCase = new GenerateTokenUseCase(shipmentProvider) 
+    httpClientProvider = new HttpClientProvider()
+    shipmentProvider = new ShipmentProvider(httpClientProvider)
+    generateTokenUseCase = new GenerateTokenUseCase(shipmentProvider)
   })
   afterEach(() => apiServerMock.resetHandlers())
   afterAll(() => apiServerMock.close())
 
   it('should not be able to generate a token when code is not defined', async () => {
-     expect(async () => await generateTokenUseCase.execute('')
-    ).rejects.toEqual(new AppError('Invalid code', 401))
+    expect(async () => await generateTokenUseCase.execute('')).rejects.toEqual(
+      new AppError('Invalid code', 401),
+    )
   })
 
   it('should be able to generate a token', async () => {
-   const jwt = await generateTokenUseCase.execute('code mock')
+    const jwt = await generateTokenUseCase.execute('code mock')
 
     expect(jwt.accessToken).toBe('access token mock')
     expect(jwt.refreshToken).toBe('refresh token mock')
- })
+  })
 })
