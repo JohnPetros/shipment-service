86570d85a1e525055d67160a4ac4a0aae0e50549
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-02 16:04:23 +0000

    ✨ feat: handle invalid token error

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,3 +1,8 @@
+import { Cache } from '@cache/index'
+import { AppErrors } from '@constants/AppErrors'
+import { CacheKeys } from '@constants/CacheKeys'
+import { IHttp } from '@http/interfaces/IHttp'
+
 export class AppError {
   public readonly message: string
   public readonly statusCode: number
@@ -5,5 +10,21 @@ export class AppError {
   constructor(message: string, statusCode = 500) {
     this.message = message
     this.statusCode = statusCode
+  }
+
+  private async handleIvalidTokenError(http: IHttp) {
+    const cache = new Cache()
+    await cache.set(CacheKeys.PREVIOUS_ROUTE, http.getPreviusRoute())
+    http.redirect('/auth/refresh_token')
+  }
+
+  async handleError(http: IHttp) {
+    switch (this.message) {
+      case AppErrors.INVALID_TOKEN:
+        await this.handleIvalidTokenError(http)
+        break
+      default:
+        http.send(this.statusCode, this.message)
+    }
   }
 }
