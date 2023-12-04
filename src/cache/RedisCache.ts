ee8ba09156b9779b1ea9a5ef886417dd31cfc659
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-04 10:32:31 +0000

    📦 build: connect to Redis service via url

--- src/cache/RedisCache.ts
+++ src/cache/RedisCache.ts
@@ -6,10 +6,7 @@ export class RedisCache implements ICache {
   private redis: Redis
 
   constructor() {
-    this.redis = new Redis({
-      port: envConfig.REDIS_PORT,
-      password: envConfig.REDIS_PASSWORD,
-    })
+    this.redis = new Redis('redis://red-clmc2e1fb9qs739b6cl0:6379')
   }
 
   async set(key: string, data: unknown): Promise<void> {
