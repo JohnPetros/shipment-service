2b4403313632c353bb205f1fc9ba66193535b4b2
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-12 23:29:22 +0000

    🧪 test: add creditCardCustomer

---
+++ src/entities/mocks/creditCardMock.ts
@@ -0,0 +1,8 @@
+import { CreditCard } from '@entities/CreditCard'
+
+export const creditCardCustomer: CreditCard = {
+  number: '4111111111111111',
+  cvv: '123',
+  expirationDate: '12/25',
+  holderName: 'John Petros',
+}
