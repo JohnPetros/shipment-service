20e3da5aafa1c57f7ccfbc2ceb207eb4eca6e661
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-12 23:33:32 +0000

    ✨ feat (utils): add error method to Console util

--- src/utils/Console.ts
+++ src/utils/Console.ts
@@ -2,4 +2,8 @@ export class Console {
   log(data: unknown) {
     console.log(JSON.stringify(data, null, 2))
   }
+
+  error(data: unknown) {
+    console.error(JSON.stringify(data, null, 2))
+  }
 }
