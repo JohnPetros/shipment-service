ef4745cea1113e2875148fa6ff53fe660bfbedf0
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-12-12 23:25:15 +0000

    🏷️ type: add address to Customer entity

--- src/entities/Customer.ts
+++ src/entities/Customer.ts
@@ -1,4 +1,16 @@
 export type Customer = {
   id: string
   email: string
+  name: string
+  phone: string
+  type: 'legal' | 'natural'
+  document: string
+  address: {
+    number: number
+    street: string
+    neighborhood: string
+    zipCode: string
+    city: string
+    state: string
+  }
 }
