a57b695aef58b0cb6e0d702c075c8381db70cc3b
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-01-01 13:59:10 +0000

    🐛 fix: resolve wrong redis connection url

--- src/cache/RedisCache.ts
+++ src/cache/RedisCache.ts
@@ -1,50 +1,50 @@
-import Redis from 'ioredis'
-import { ICache } from './ICache'
-import { AppError } from '@utils/AppError'
-import { envConfig } from '@configs/envConfig'
+import Redis from "ioredis";
+import { ICache } from "./ICache";
+import { AppError } from "@utils/AppError";
+import { envConfig } from "@configs/envConfig";
 
 export class RedisCache implements ICache {
-  private redis: Redis
+  private redis: Redis;
 
   constructor() {
-    const URL = envConfig.REDIS_EXTERNAL_URL
-    const PORT = envConfig.REDIS_PORT
-    const NODE_ENV = envConfig.NODE_ENV
+    const URL = envConfig.REDIS_EXTERNAL_URL;
+    const PORT = envConfig.REDIS_PORT;
+    const NODE_ENV = envConfig.NODE_ENV;
 
-    if (NODE_ENV === 'development') {
+    if (NODE_ENV === "development") {
       try {
         this.redis = new Redis({
           port: PORT,
-        })
+        });
       } catch (error) {
-        throw new AppError('Redis connection error')
+        throw new AppError("Redis connection error");
       }
     }
 
-    if (NODE_ENV === 'production') {
-      if (!URL) throw new AppError('Redis connection url is not provided')
+    if (NODE_ENV === "production") {
+      if (!URL) throw new AppError("Redis connection url is not provided");
 
       try {
-        this.redis = new Redis(URL)
+        this.redis = new Redis(URL);
       } catch (error) {
-        throw new AppError('Redis connection error')
+        throw new AppError("Redis connection error");
       }
     }
 
-    this.redis = new Redis()
+    this.redis = new Redis();
   }
 
   async set(key: string, data: unknown): Promise<void> {
-    await this.redis.set(key, JSON.stringify(data))
+    await this.redis.set(key, JSON.stringify(data));
   }
 
   async get<Data>(key: string): Promise<Data | null> {
-    const data = await this.redis.get(key)
-    if (data) return JSON.parse(data)
-    else return null
+    const data = await this.redis.get(key);
+    if (data) return JSON.parse(data);
+    else return null;
   }
 
   async delete(key: string): Promise<void> {
-    await this.redis.del(key)
+    await this.redis.del(key);
   }
 }
