fa98fb8152ea173fca71ddb4da2b569597bd0d82
johnpetros <joaopedro.nc@outlook.com>
2024-03-03 14:49:06 -0300

    🔧 chore: add redis URL connection

--- src/cache/RedisCache.ts
+++ src/cache/RedisCache.ts
@@ -1,23 +1,31 @@
 import Redis from 'ioredis'
 import { ICache } from './ICache'
 import { AppError } from '@utils/AppError'
-import { envConfig } from '@configs/envConfig'
+
+const URL = process.env.NODE_ENV === 'development'
+  ? process.env.REDIS_EXTERNAL_URL
+  : process.env.REDIS_INTERNAL_URL
+
+
+// redis://red-clmc2e1fb9qs739b6cl0:iXwYwU7pV675NNAegTbmoyABnProb758@oregon-redis.render.com:6379
+
 
 export class RedisCache implements ICache {
   private redis: Redis
 
   constructor() {
-    const PORT = envConfig.REDIS_PORT
-
-    if (!PORT) throw new AppError('Redis connection PORT is not provided')
+    if (!URL) throw new AppError('Redis url connection is not provided')
 
     try {
-      this.redis = new Redis({ port: PORT })
+      const client = this.redis = new Redis(URL)
+      client.on('error', error => {
+        console.log(error);
+      });
     } catch (error) {
       throw new AppError('Redis connection error')
     }
 
-    this.redis = new Redis()
+    // this.redis = new Redis(URL)
   }
 
   async set(key: string, data: unknown): Promise<void> {
