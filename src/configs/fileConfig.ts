4ec33e793fa7b9faf4342ab254f88bb94c8b4b6c
johnpetros <joaopedro.nc@outlook.com>
2024-03-03 17:19:32 -0300

    🔧 chore: add certificates folder and ssl files to fileConfig

--- src/configs/fileConfig.ts
+++ src/configs/fileConfig.ts
@@ -2,6 +2,12 @@ import path from 'node:path'
 
 export const fileConfig = {
   FOLDERS: {
-    TMP: path.resolve(__dirname, '..', '.tmp'),
+    TMP: path.resolve(__dirname, '..', '..', '.tmp'),
+    CERTIFICATES: path.resolve(__dirname, '..', '..', 'certificates'),
   },
+  FILES: {
+    SSL_KEY: 'ssl.key.pem',
+    SSL_CRT: 'ssl.crt.pem',
+    SSL_CSR: 'ssl.csr.pem',
+  }
 }
